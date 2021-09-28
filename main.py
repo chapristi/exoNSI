7a165968390f52c1b3097bcce7400de5f7b394e2
chapristi <70172942+chapristi@users.noreply.github.com>
2021-09-28 14:48:20 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+"""
+def moyenne (serie):
+  if(len(serie) == 0):
+    return "aucune valeur n'a etait entrée"
+  else:
+    for i in serie:
+      somme = 0
+      somme += i 
+      return somme / len(serie)
+
+print(moyenne([]))
+"""
+"""
+def NombreOccurence(chaine,car):
+  compteur = 0
+  for i in chaine:
+    if i == car:
+      compteur += 1
+  return compteur
+print(NombreOccurence("aazdughayizdg","a"))
+"""
+"""
+
+def PositionChar(chaine,car):
+  position = []
+  for i in range(len(chaine)):
+    if chaine[i] == car:
+       position.append(i)
+  return position
+print(PositionChar("ozoerzoeroo","o"))
+"""
+
+"""
+def repondre (nom,annee,mois):
+  print(f" vous vous nommez {nom} et vous avez {(2021 - annee )} ans ")
+
+nom = input("votre nom")
+annee = int(input("année de naissance"))
+moi =input("votre moi de naissance ")
+repondre(nom,annee,moi)
+"""
+def TableMulti():
+ 
+
+TableMulti()
+
+
